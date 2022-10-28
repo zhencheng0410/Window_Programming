92c57cb5e2eb51e94364f03c8accb81e19c71edb
zhencheng0410 <opojfkoke410@gmail.com>
2022-10-29 02:45:39 +0800

    practice 6_2

---
+++ Phone/Phone/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Phone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(21, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(907, 502);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(899, 473);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(29, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(864, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(899, 473);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(789, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 33);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(688, 24);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(17, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Save";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(8, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(885, 398);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 549);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Phone/Phone/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace Phone
+{
+    public partial class Form1 : Form
+    {
+        FileInfo[] Sound1 = new FileInfo[4];
+        FileInfo[] Sound2 = new FileInfo[3];
+        SoundPlayer player1 = new SoundPlayer();
+        SoundPlayer player2 = new SoundPlayer();
+        Button[] btn = new Button[15];
+        Image[] image_list = new Image[16];
+        Boolean one = true;
+        int index;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabPage1.Text = "Telephone";
+            tabPage2.Text = "Log";
+            label1.Text = "Telephone";
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            this.KeyUp += new KeyEventHandler(Form1_KeyUp);
+            for (int i = 0; i < 15; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Width = 50;
+                btn[i].Height = 50;
+                btn[i].Left = 250 + 60 * (i % 3);
+                btn[i].Top = 70 + 60 * (i / 3);
+                tabPage1.Controls.Add(btn[i]);
+                btn[i].Name = i.ToString();
+                btn[i].Click += new EventHandler(btn_click);
+                btn[i].Font = new Font("新細明體", 20F, FontStyle.Bold);
+                if (i >= 0 && i <= 8)
+                    btn[i].Text = (i + 1).ToString();
+                else if (i == 9)
+                    btn[i].Text = "*";
+                else if (i == 10)
+                    btn[i].Text = "0";
+                else if (i == 11)
+                    btn[i].Text = "#";
+                else if (i == 12)
+                    btn[i].Text = "❌";
+                else if (i == 13)
+                    btn[i].Text = "📱";
+                else if (i == 14)
+                    btn[i].Text = "⬅";
+            }
+            Sound1[0] = new FileInfo("../../audio/697.wav");
+            Sound1[1] = new FileInfo("../../audio/770.wav");
+            Sound1[2] = new FileInfo("../../audio/852.wav");
+            Sound1[3] = new FileInfo("../../audio/941.wav");
+
+            Sound2[0] = new FileInfo("../../audio/1209.wav");
+            Sound2[1] = new FileInfo("../../audio/1336.wav");
+            Sound2[2] = new FileInfo("../../audio/1477.wav");
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            one = true;
+            player1.Stop();
+            player2.Stop();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (label1.Text == "Telephone")
+                label1.Text = "";
+
+            if (e.Shift)
+            {
+                if (e.KeyCode == Keys.D3)
+                {
+                    if (one)
+                    {
+                        label1.Text += "#";
+                        index = 11;
+                    }
+                    one = false;
+                }
+                else if (e.KeyCode == Keys.D8)
+                {
+                    if (one)
+                    {
+                        label1.Text += "*";
+                        index = 9;
+                    }
+                    one = false;
+                }
+                PlaySound(index);
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                if (one)
+                {
+                    if (btn[0].Enabled)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            if (i != 13)
+                            {
+                                btn[i].Enabled = false;
+                            }
+                        }
+                        btn[13].Text = "📵";
+                    }
+                    else if (!btn[0].Enabled)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            if (i != 13)
+                            {
+                                btn[i].Enabled = true;
+                            }
+                        }
+                        textBox1.Text += label1.Text += "\r\n";
+                        label1.Text = "Telephone";
+                        btn[13].Text = "📱";
+                    }
+                }
+                one = false;
+            }
+            else if (e.KeyCode == Keys.Back)
+            {
+                if (one)
+                    label1.Text = label1.Text.Substring(0, Math.Max(0, label1.Text.Length - 1));
+                one = false;
+            }
+            else if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+            {
+                if (one)
+                {
+                    label1.Text += e.KeyCode.ToString()[1];
+                    index = Convert.ToInt16(e.KeyCode.ToString()[1]) - Convert.ToInt16('0');
+                    if (index >= 1 && index <= 9)
+                    {
+                        index -= 1;
+                    }
+                    else if (index == 0)
+                    {
+                        index = 10;
+                    }
+                }
+                PlaySound(index);
+                one = false;
+            }
+        }
+        private void btn_click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            int index = Convert.ToInt16(bt.Name);
+            if (label1.Text == "Telephone")
+                label1.Text = "";
+            switch (index)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    label1.Text += (index + 1).ToString();
+                    break;
+                case 9:
+                    label1.Text += "*";
+                    break;
+                case 10:
+                    label1.Text += "0";
+                    break;
+                case 11:
+                    label1.Text += "#";
+                    break;
+                case 12:
+                    label1.Text = "";
+                    break;
+                case 13:
+                    if (btn[0].Enabled)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            if (i != 13)
+                            {
+                                btn[i].Enabled = false;
+                            }
+                        }
+                        btn[13].Text = "📵";
+                    }
+                    else if (!btn[0].Enabled)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            if (i != 13)
+                            {
+                                btn[i].Enabled = true;
+                            }
+                        }
+                        textBox1.Text += label1.Text += "\r\n";
+                        label1.Text = "Telephone";
+                        btn[13].Text = "📱";
+                    }
+                    break;
+                case 14:
+                    label1.Text = label1.Text.Substring(0, Math.Max(0, label1.Text.Length - 1));
+                    break;
+            }
+            if (index >= 0 && index <= 11)
+            {
+                PlaySound(index);
+            }
+        }
+        private void PlaySound(int index)
+        {
+            player1.SoundLocation = Sound1[index / 3].ToString();
+            player1.Play();
+            player2.SoundLocation = Sound2[index % 3].ToString();
+            player2.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Empty String");
+            }
+            else if (!File.Exists(textBox2.Text))
+            {
+                MessageBox.Show("Directory Not Found");
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(textBox2.Text);
+                sw.Write(textBox1.Text);
+                sw.Close();
+                FileInfo fi = new FileInfo(textBox2.Text);
+                MessageBox.Show("Done\n" + textBox2.Text);
+            }
+        }
+    }
+}
