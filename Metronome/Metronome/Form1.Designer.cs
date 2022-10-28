bb1a8bf95f84ea3a110b4ab2b2dd54edd14ca90c
zhencheng0410 <opojfkoke410@gmail.com>
2022-10-29 02:40:40 +0800

    practice 6_1

---
+++ Metronome/Metronome/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Metronome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 412);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 15);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Beat Emphasis";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 40);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(483, 350);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 23;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(285, 350);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 22;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(226, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(416, 211);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 133);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(112, 152);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(557, 51);
+            this.panel2.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(112, 152);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(557, 51);
+            this.panel1.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(303, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 40);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(108, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "60";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(633, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "180";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(155, 90);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(457, 56);
+            this.trackBar1.TabIndex = 14;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(379, 409);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 26;
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 560);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Metronome/Metronome/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Metronome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SoundPlayer ding = new SoundPlayer();
+        SoundPlayer dong = new SoundPlayer();
+        SoundPlayer doo = new SoundPlayer();
+        Button[] btn = new Button[12];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            trackBar1.Maximum = 180;
+            trackBar1.Minimum = 60;
+            trackBar1.Value = 120;
+            panel2.Visible = false;
+            panel1.Visible = true;
+            for (int i = 0; i < 4; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Width = 40;
+                btn[i].Height = 40;
+                btn[i].Left = 10 + 120 * i;
+                panel1.Controls.Add(btn[i]);
+                btn[i].Name = i.ToString();
+            }
+            for (int i = 4; i < 12; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Width = 40;
+                btn[i].Height = 40;
+                btn[i].Left = 10 + 50 * (i - 4);
+                panel2.Controls.Add(btn[i]);
+                btn[i].Name = i.ToString();
+            }
+            radioButton1.Checked = true;
+            comboBox1.Text = "4";
+            ding.SoundLocation = Application.StartupPath + "\\ding.wav";
+            ding.Load();
+            dong.SoundLocation = Application.StartupPath + "\\dong.wav";
+            dong.Load();
+            doo.SoundLocation = Application.StartupPath + "\\doo.wav";
+            doo.Load();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label3.Text = trackBar1.Value.ToString() + "BPM";
+            if (radioButton1.Checked == true)
+                timer1.Interval = 60000 / trackBar1.Value;
+            else if (radioButton2.Checked == true)
+                timer1.Interval = 30000 / trackBar1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Start")
+            {
+                button1.Text = "Stop";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                button1.Text = "Start";
+                timer1.Enabled = false; ;
+                x = 0;
+                for (int i = 0; i < 12; i++) btn[i].BackColor = Color.White;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 60000 / trackBar1.Value;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 30000 / trackBar1.Value;
+        }
+
+        int x = 0, y = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "4")
+            {
+                if (radioButton2.Checked)
+                {
+                    if (y % 2 == 0)
+                    {
+                        if (x != 0)
+                            btn[x - 1].BackColor = Color.White;
+                        else
+                            btn[3].BackColor = Color.White;
+                        btn[x].BackColor = Color.LightGreen;
+                    }
+                }
+                else
+                {
+                    if (x != 0)
+                        btn[x - 1].BackColor = Color.White;
+                    else
+                        btn[3].BackColor = Color.White;
+                    btn[x].BackColor = Color.LightGreen;
+                }
+            }
+            else if (comboBox1.Text == "8")
+            {
+                if (radioButton2.Checked)
+                {
+                    if (y % 2 == 0)
+                    {
+                        if (x != 0)
+                            btn[x + 3].BackColor = Color.White;
+                        else
+                            btn[11].BackColor = Color.White;
+                        btn[x + 4].BackColor = Color.LightGreen;
+                    }
+                }
+                else
+                {
+                    if (x != 0)
+                        btn[x + 3].BackColor = Color.White;
+                    else
+                        btn[11].BackColor = Color.White;
+                    btn[x + 4].BackColor = Color.LightGreen;
+                }
+            }
+            if (x == 0)
+            {
+                if (radioButton2.Checked)
+                {
+                    if (y % 2 == 0)
+                    {
+                        ding.Load();
+                        ding.Play();
+                    }
+                    else
+                    {
+                        doo.Load();
+                        doo.Play();
+                    }
+                }
+                else
+                {
+                    ding.Load();
+                    ding.Play();
+                }
+            }
+            else if (x != 0)
+            {
+                if (radioButton2.Checked)
+                {
+                    if (y % 2 == 0)
+                    {
+                        dong.Load();
+                        dong.Play();
+                    }
+                    else
+                    {
+                        doo.Load();
+                        doo.Play();
+                    }
+                }
+                else
+                {
+                    dong.Load();
+                    dong.Play();
+                }
+            }
+            y++;
+            if (radioButton2.Checked)
+            {
+                if (y % 2 == 0)
+                {
+                    x++;
+                }
+            }
+            else x++;
+            if (x == Convert.ToInt16(comboBox1.Text)) x = 0;
+            if (y == Convert.ToInt16(comboBox1.Text)) y = 0;
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "4")
+            {
+                panel1.Visible = true;
+                panel2.Visible = false;
+            }
+            else if (comboBox1.Text == "8")
+            {
+                panel1.Visible = false;
+                panel2.Visible = true;
+            }
+        }
+    }
+}
