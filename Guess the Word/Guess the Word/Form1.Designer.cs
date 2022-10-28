646e6509c8db40988b126545fe31a11405e92e96
zhencheng0410 <opojfkoke410@gmail.com>
2022-10-29 02:14:39 +0800

    practice 5_1

---
+++ Guess the Word/Guess the Word/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Guess_the_Word
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(34, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(683, 426);
+            this.panel1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(228, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "請輸入要猜的單字:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(230, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 72);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "猜英文單字\r\n6次猜錯機會\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(-6, 11);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(813, 426);
+            this.panel2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(306, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 28);
+            this.label5.TabIndex = 2;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(595, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "猜錯次數: 0次";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(595, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "時間:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Guess the Word/Guess the Word/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_the_Word
+{
+    public partial class Form1 : Form
+    {
+        Button[] btn = new Button[26];
+        int x = 0;
+        Button bt;
+        int appear = 0, wrong = 0, success = 0, num = 0;
+        int[] isappear = new int[26];
+        string objective, guess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            objective = textBox1.Text.ToUpper();
+            char[] words = objective.ToCharArray();
+
+            foreach (char word in words)
+            {
+                if (word < 'A' || word > 'Z')
+                {
+                    textBox1.Clear();
+                }
+            }
+            if (textBox1.Text != "")
+            {
+                panel2.Visible = true;
+                panel1.Visible = false;
+                timer1.Enabled = true;
+                objective = textBox1.Text.ToUpper();
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    label5.Text += "_ ";
+                    guess = label5.Text;
+                }
+                textBox1.Enabled = false;
+                this.KeyPreview = true;
+                this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            label3.Text = "時間: " + num.ToString();
+            num++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Left = Convert.ToInt16(0.3 * (panel2.Width - label5.Width));
+            panel2.Visible = false;
+            timer1.Enabled = false;
+            for (int i = 0; i < 26; i++)
+            {
+                btn[i] = new Button();
+                panel2.Controls.Add(btn[i]);
+                btn[i].Width = 30;
+                btn[i].Height = 30;
+                if (i >= 0 && i <= 9)
+                {
+                    btn[i].Top = 30;
+                    btn[i].Left = 75 + 35 * (i % 10);
+                }
+                if (i >= 10 && i <= 19)
+                {
+                    btn[i].Top = 70;
+                    btn[i].Left = 75 + 35 * (i % 10);
+                }
+                if (i >= 20 && i <= 25)
+                {
+                    btn[i].Top = 110;
+                    btn[i].Left = 145 + 35 * (i % 10);
+                }
+                btn[i].Visible = true;
+                btn[i].Text = Convert.ToChar(i + 'A').ToString();
+                btn[i].Click += new EventHandler(btn_Click);
+                btn[i].Name = i.ToString();
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 'a' && e.KeyChar <= 'z')
+            {
+                e.KeyChar = Convert.ToChar(e.KeyChar.ToString().ToUpper());
+            }
+            x = e.KeyChar - 'A';
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (objective[i] == e.KeyChar && isappear[i] == 0)
+                {
+                    appear = 1;
+                    btn[x].BackColor = Color.LightGreen;
+                    success += 1;
+                    guess = guess.Remove(2 * i, 1);
+                    guess = guess.Insert(2 * i, Convert.ToChar(x + 'A').ToString());
+                    label5.Text = guess;
+                    isappear[i] = 1;
+                }
+                else if (objective[i] == e.KeyChar)
+                {
+                    appear = 1;
+                }
+            }
+            if (appear == 0)
+            {
+                btn[x].Visible = false;
+                wrong += 1;
+                label4.Text = "猜錯次數: " + wrong + "次";
+            }
+            DialogResult result;
+            appear = 0;
+            if (wrong == 6)
+            {
+                label5.Text = textBox1.Text.ToUpper();
+                timer1.Enabled = false;
+                result = MessageBox.Show("You Lose!");
+                if (result == DialogResult.OK)
+                {
+                    restart();
+                }
+            }
+            else if (success == textBox1.Text.Length)
+            {
+                timer1.Enabled = false;
+                result = MessageBox.Show("花費時間:" + num.ToString() + "\r\n猜錯" + wrong + "次", "You Win!");
+                if (result == DialogResult.OK)
+                {
+                    restart();
+                }
+            }
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            bt = (Button)sender;
+            x = Convert.ToInt16(bt.Name);
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (objective[i] == x + 'A')
+                {
+                    appear = 1;
+                    btn[x].BackColor = Color.LightGreen;
+                    success += 1;
+                    guess = guess.Remove(2 * i, 1);
+                    guess = guess.Insert(2 * i, Convert.ToChar(x + 'A').ToString());
+                    label5.Text = guess;
+                }
+            }
+            if (appear == 0)
+            {
+                btn[x].Visible = false;
+                wrong += 1;
+                label4.Text = "猜錯次數: " + wrong + "次";
+            }
+            DialogResult result;
+            appear = 0;
+            if (wrong == 6)
+            {
+                timer1.Enabled = false;
+                result = MessageBox.Show("You Lose!");
+                if (result == DialogResult.OK)
+                {
+                    restart();
+                }
+            }
+            else if (success == textBox1.Text.Length)
+            {
+                timer1.Enabled = false;
+                result = MessageBox.Show("花費時間:" + num.ToString() + "\r\n猜錯" + wrong + "次", "You Win!");
+                if (result == DialogResult.OK)
+                {
+                    restart();
+                }
+            }
+        }
+        private void restart()
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                btn[i].Visible = true;
+                btn[i].BackColor = Color.White;
+                panel2.Visible = false;
+                panel1.Visible = true;
+                textBox1.Text = "";
+                num = 0;
+                wrong = 0;
+                success = 0;
+                label3.Text = "時間: " + num.ToString();
+                label4.Text = "猜錯次數: " + wrong + "次";
+                guess = "";
+                label5.Text = guess;
+                isappear[i] = 0;
+            }
+            textBox1.Enabled = true;
+            this.KeyPreview = false;
+            this.KeyPress -= new KeyPressEventHandler(Form1_KeyPress);
+        }
+    }
+}
